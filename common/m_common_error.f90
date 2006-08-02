d77d1f3c53da06238f5dd2b7e6fc831664cd8bec
Toby White <tow@toby-whites-ibook-g4.local>
2006-08-02 23:43:28 +0100

    Reformat last of common/ files

--- common/m_common_error.f90
+++ common/m_common_error.f90
@@ -1,61 +1,61 @@
 module m_common_error
 
-use pxf, only: pxfabort, pxfflush
+  use pxf, only: pxfabort, pxfflush
 
-implicit none
+  implicit none
 
-interface FoX_warning
-  module procedure FoX_warning_base
-end interface
+  interface FoX_warning
+    module procedure FoX_warning_base
+  end interface
 
-interface FoX_error
-  module procedure FoX_error_base
-end interface
+  interface FoX_error
+    module procedure FoX_error_base
+  end interface
 
-interface FoX_fatal
-  module procedure FoX_fatal_base
-end interface
+  interface FoX_fatal
+    module procedure FoX_fatal_base
+  end interface
 
 contains
-!---------------------------------------------------------
-! Error handling/trapping routines:
+  !---------------------------------------------------------
+  ! Error handling/trapping routines:
 
-    subroutine FoX_warning_base(msg)
-      ! Emit warning, but carry on.
-      character(len=*), intent(in) :: msg
+  subroutine FoX_warning_base(msg)
+    ! Emit warning, but carry on.
+    character(len=*), intent(in) :: msg
 
-      write(0,'(a)') 'WARNING(FoX)'
-      write(0,'(a)')  msg
-      call pxfflush(0)
+    write(0,'(a)') 'WARNING(FoX)'
+    write(0,'(a)')  msg
+    call pxfflush(0)
 
-    end subroutine FoX_warning_base
+  end subroutine FoX_warning_base
 
-    subroutine FoX_error_base(msg)
-      ! Emit error message and stop.
-      ! No clean up is done here, but this can
-      ! be overridden to include clean-up routines
-      character(len=*), intent(in) :: msg
+  subroutine FoX_error_base(msg)
+    ! Emit error message and stop.
+    ! No clean up is done here, but this can
+    ! be overridden to include clean-up routines
+    character(len=*), intent(in) :: msg
 
-      write(0,'(a)') 'ERROR(FoX)'
-      write(0,'(a)')  msg
-      call pxfflush(0)
+    write(0,'(a)') 'ERROR(FoX)'
+    write(0,'(a)')  msg
+    call pxfflush(0)
 
-      stop
+    stop
 
-    end subroutine FoX_error_base
+  end subroutine FoX_error_base
 
-    subroutine FoX_fatal_base(msg)
-      !Emit error message and abort with coredump.
-      !No clean-up occurs
+  subroutine FoX_fatal_base(msg)
+    !Emit error message and abort with coredump.
+    !No clean-up occurs
 
-      character(len=*), intent(in) :: msg
+    character(len=*), intent(in) :: msg
 
-      write(0,'(a)') 'ABORT(FOX)'
-      write(0,'(a)')  msg
-      call pxfflush(0)
+    write(0,'(a)') 'ABORT(FOX)'
+    write(0,'(a)')  msg
+    call pxfflush(0)
 
-      call pxfabort()
+    call pxfabort()
 
-    end subroutine FoX_fatal_base
+  end subroutine FoX_fatal_base
 
 end module m_common_error
