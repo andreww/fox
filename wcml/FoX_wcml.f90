d950b76477bbed07ef66669a0648fef89d58686c
Toby White <tow21@cam.ac.uk>
2006-08-02 12:21:48 +0100

    Publically export all wcml functions

--- wcml/FoX_wcml.f90
+++ wcml/FoX_wcml.f90
@@ -3,6 +3,8 @@ module FoX_wcml
   use m_wcml_core
   use m_wcml_coma
   use m_wcml_geometry
+  use m_wcml_lattice
+  use m_wcml_metadata
   use m_wcml_molecule
   use m_wcml_parameter
   use m_wcml_property
@@ -14,6 +16,19 @@ module FoX_wcml
   public :: cmlStartCml
   public :: cmlEndCml
 
+  public :: cmlStartModule
+  public :: cmlEndModule
+
+  public :: cmlStartStep
+  public :: cmlEndStep
+
+  public :: cmlAddLength
+  public :: cmlAddAngle
+  public :: cmlAddTorsion
+
+  public :: cmlAddCrystal
+  public :: cmlAddLattice
+
   public :: cmlStartPropertyList
   public :: cmlEndPropertyList
   public :: cmlAddProperty
@@ -22,6 +37,20 @@ module FoX_wcml
   public :: cmlEndParameterList
   public :: cmlAddParameter
 
+  public :: cmlStartMetadataList
+  public :: cmlEndMetadataList
+  public :: cmlAddMetadata
+
   public :: cmlAddMolecule
+
+  public :: cmlAddBand
+  public :: cmlStartBandList
+  public :: cmlEndBandList
+
+  public :: cmlAddKPoint
+  public :: cmlStartKPointList
+  public :: cmlEndKPointList
+
+  public :: cmlAddEigenvalue
 
 end module FoX_wcml
