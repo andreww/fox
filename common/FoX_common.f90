957d4868ba16b6a57135f7c915f6a0db7e41d113
Toby White <tow21@cam.ac.uk>
2007-10-15 10:51:41 +0100

    COMMON: version 3.0

--- common/FoX_common.f90
+++ common/FoX_common.f90
@@ -6,7 +6,7 @@ module FoX_common
   implicit none
   private
 
-  character(len=*), parameter :: FoX_version = '2.2'
+  character(len=*), parameter :: FoX_version = '3.0'
 
   public :: FoX_version
 
