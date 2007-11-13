500f3741f604a61e2e9fed650698350850053c79
Toby White <tow21@cam.ac.uk>
2007-11-13 13:54:28 +0000

    Fix up wcml testsuite

--- wcml/test/test.sh
+++ wcml/test/test.sh
@@ -4,34 +4,37 @@
 # have Unix line endings only by using 'tr', in
 # order to compare properly to our canonical versions.
 
-make $1.exe
-./$1.exe 2>&1 | tr -d '\15' > test.out
 passed=no
-if [ -f $1.xml ]
-then
-  grep -v UUID test.xml | tr -d '/15' > test.xml.tmp; mv test.xml.tmp test.xml
-  if diff test.xml $1.xml > /dev/null; then
-     echo $1 >> failed.out
-     echo "------------" >> failed.out
-     diff test.xml $1.xml >> failed.out
-     echo "------------" >> failed.out
-  else
-    passed=yes
-  fi
-elif [ -f $1.out ]
-then
-# Note below that we don't do a direct grep; we just check 
-# that the only DIFFerences are in one direction.
-  if diff test.out $1.out | grep "^>" > /dev/null; then
-     echo $1 >> failed.out
-     echo "------------" >> failed.out
-     diff test.out $1.out >> failed.out
-     echo "------------" >> failed.out
-  else
-    passed=yes
-  fi
+if ! make $1.exe; then
+  echo Cannot compile $1
 else
-  echo No test output found for $1
+  ./$1.exe 2>&1 | tr -d '\15' > test.out
+  if [ -f $1.xml ]
+  then
+    grep -v UUID test.xml | tr -d '/15' > test.xml.tmp; mv test.xml.tmp test.xml
+    if diff test.xml $1.xml > /dev/null; then
+      echo $1 >> failed.out
+      echo "------------" >> failed.out
+      diff -u test.xml $1.xml >> failed.out
+      echo "------------" >> failed.out
+    else
+       passed=yes
+    fi
+  elif [ -f $1.out ]
+  then
+  # Note below that we don't do a direct grep; we just check 
+  # that the only DIFFerences are in one direction.
+    if diff test.out $1.out | grep "^>" > /dev/null; then
+      echo $1 >> failed.out
+      echo "------------" >> failed.out
+      diff test.out $1.out >> failed.out
+      echo "------------" >> failed.out
+    else
+      passed=yes
+    fi
+  else
+    echo No test output found for $1
+  fi
 fi
 
 if [ $passed = yes ]; then
