5cb5ddd13700198291c0cdb6a59ca6424a26fa19
Toby White <tow21@cam.ac.uk>
2007-11-14 10:17:14 +0000

    Fix sax tests to pass with NAG

--- sax/test/test_sax_reader_1.f90
+++ sax/test/test_sax_reader_1.f90
@@ -5,10 +5,12 @@ program test_sax_reader
   type(file_buffer_t) :: fb
 
   integer :: iostat
+  character(1) :: c
 
   call open_file(fb, file="test_sax_reader_1.in",  iostat=iostat)
 
-  write(*,'(2a)') 'char:', read_char(fb, iostat)
+  c = read_char(fb, iostat)
+  write(*,'(2a)') 'char:', c
   write(*,'(a,i0)') 'iost:', iostat
 
   call close_file(fb) 
--- sax/test/test_sax_reader_2.f90
+++ sax/test/test_sax_reader_2.f90
@@ -5,10 +5,12 @@ program test_sax_reader
   type(file_buffer_t) :: fb
 
   integer :: iostat
+  character(3) :: c
 
   call open_file(fb, file="test_sax_reader_2.in", iostat=iostat)
 
-  write(*,'(2a)') 'char:', read_chars(fb, 3, iostat)
+  c = read_chars(fb, 3, iostat)
+  write(*,'(2a)') 'char:', c
   write(*,'(a,i0)') 'iost:', iostat
 
   call close_file(fb) 
