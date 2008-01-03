9ce4e155730721e38984aba6cbaa51e7467a5abd
Toby White <tow@toby-whites-macbook.local>
2008-01-03 02:14:43 +0000

    Update WXML to match internal API

--- wxml/m_wxml_core.F90
+++ wxml/m_wxml_core.F90
@@ -440,7 +440,7 @@ contains
         if (.not.checkExistingRefs()) &
           call wxml_warning(xf, "Reference to unknown parameter entity")
       endif
-      call register_internal_PE(xf%xds, name, PEdef)
+      call register_internal_PE(xf%xds, name=name, text=PEdef, wfc=.false.)
     else
       if (.not.checkSystemID(system)) &
         call wxml_fatal("Parameter entity System ID is invalid: "//system)
@@ -448,7 +448,7 @@ contains
         if (.not.checkPublicID(public)) &
           call wxml_fatal("Parameter entity Public ID is invalid: "//public)
       endif
-      call register_external_PE(xf%xds, name, system, public)
+      call register_external_PE(xf%xds, name=name, systemId=system, publicId=public, wfc=.false.)
     endif
 
     call add_eol(xf)
@@ -531,7 +531,7 @@ contains
 
     if (.not.checkName(name, xf%xds)) &
       call wxml_error("xml_AddInternalEntity: Invalid Name: "//name)
-    call register_internal_GE(xf%xds, name, value)
+    call register_internal_GE(xf%xds, name=name, text=value, wfc=.false.)
 
     call add_eol(xf)
     
@@ -598,7 +598,8 @@ contains
         call wxml_error("xml_AddExternalEntity: Invalid Public: "//public)
     endif
     ! Notation only needs checked if not already registered - done above.
-    call register_external_GE(xf%xds, name, system, public, notation)
+    call register_external_GE(xf%xds, name=name, &
+      systemID=system, publicId=public, notation=notation, wfc=.false.)
     
     call add_eol(xf)
     
