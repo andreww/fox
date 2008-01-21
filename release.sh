3a7a98812161263fb04c630831d21cc1daa8fdf6
Toby White <tow21@cam.ac.uk>
2008-01-21 11:18:06 +0000

    Put cutdown version somewhere sensible

--- release.sh
+++ release.sh
@@ -5,10 +5,11 @@ git-archive --format=tar --prefix=FoX-3.1.2/ HEAD | gzip -9 > ../FoX-3.1.2-full.
 mkdir tmpFoX
 cd tmpFoX
 tar xzf ../../FoX-3.1.2-full.tar.gz
-cd FoX-3.1.2
-make cutdown
-cd ..
-tar czf FoX-3.1.2.tar.gz FoX-3.1.2
+(
+  cd FoX-3.1.2
+  make cutdown
+)
+tar czf ../../FoX-3.1.2.tar.gz FoX-3.1.2
 rm -rf FoX-3.1.2
 
 for i in wxml wcml sax dom
