43bc5df0beb9968b10062900b80fab4889e9fa24
Toby White <tow@uszla.me.uk>
2008-07-22 17:02:02 +0100

    Update version number

--- common/FoX_common.F90
+++ common/FoX_common.F90
@@ -9,9 +9,9 @@ module FoX_common
   private
 
 #ifdef DUMMYLIB
-  character(len=*), parameter :: FoX_version = '4.0.1-dummy'
+  character(len=*), parameter :: FoX_version = '4.0.2-dummy'
 #else
-  character(len=*), parameter :: FoX_version = '4.0.1'
+  character(len=*), parameter :: FoX_version = '4.0.2'
 #endif
 
   public :: FoX_version
