andreww/fox

commit 3e0390d530db62f73e6136002d550390b3a998b9

Make normalize_attribute_text work with vstr's
This function now takes an automatic length fortran
string as input and returns a pointer to a vstr.
This seems to be the cleanest way to do this at the
moment as it is called from places that have both
vstr's and arrays of chars. Internally the function
still uses arrays of chars.

sax/m_sax_tokenizer.F90
@@ -858,10 +858,11 @@ contains
 
   recursive function normalize_attribute_text(fx, s_in) result(s_out)
     type(sax_parser_t), intent(inout) :: fx
-    character, dimension(:), intent(in) :: s_in
-    character, dimension(:), pointer :: s_out
+    character(len=*), intent(in) :: s_in
+    type(vs), pointer :: s_out
 
-    character, dimension(:), pointer :: s_temp, s_temp2, s_ent, tempString
+    character, dimension(:), pointer :: s_temp, s_temp2, tempString
+    type(vs), pointer :: s_ent
     character :: dummy
     integer :: i, i2, j
     type(entity_t), pointer :: ent
@@ -874,27 +875,27 @@ contains
     ! Expand all &
     ! Complain about < and &
 
-    allocate(s_temp(size(s_in))) ! in the first instance
-    allocate(s_out(0)) ! in case we return early ...
+    allocate(s_temp(len(s_in))) ! in the first instance
+    s_out => new_vs() ! in case we return early ...
     s_ent => null()
     tempString => null()
 
     i2 = 1
     i = 1
     do 
-      if (i > size(s_in)) exit
+      if (i > len(s_in)) exit
       ! Firstly, all whitespace must become 0x20
-      if (verify(s_in(i),XML_WHITESPACE)==0) then
+      if (verify(s_in(i:i),XML_WHITESPACE)==0) then
         s_temp(i2) = " "
         ! Then, < is always illegal
         i = i + 1
         i2 = i2 + 1
-      elseif (s_in(i)=='<') then
+      elseif (s_in(i:i)=='<') then
         call add_error(fx%error_stack, "Illegal < found in attribute.")
         goto 100
         ! Then, expand <
-      elseif (s_in(i)=='&') then
-        j = index(str_vs(s_in(i+1:)), ';')
+      elseif (s_in(i:i)=='&') then
+        j = index(s_in(i+1:), ';')
         if (j==0) then
           call add_error(fx%error_stack, "Illegal & found in attribute")
           goto 100
@@ -903,7 +904,7 @@ contains
           goto 100
         endif
         allocate(tempString(j-1))
-        tempString = s_in(i+1:i+j-1)
+        tempString = vs_str_alloc(s_in(i+1:i+j-1))
         if (existing_entity(fx%predefined_e_list, str_vs(tempString))) then
           ! Expand immediately
           s_temp(i2) = expand_entity_text(fx%predefined_e_list, str_vs(tempString))
@@ -939,20 +940,20 @@ contains
 #endif
             ! Recursively expand entity, checking for errors.
             s_ent => normalize_attribute_text(fx, &
-              vs_str(expand_entity_text(fx%xds%entityList, str_vs(tempString))))
+              (expand_entity_text(fx%xds%entityList, str_vs(tempString))))
             dummy = pop_entity_list(fx%forbidden_ge_list)
             if (in_error(fx%error_stack)) then
               goto 100
             endif
-            allocate(s_temp2(size(s_temp)+size(s_ent)-j))
+            allocate(s_temp2(size(s_temp)+len(s_ent)-j))
             s_temp2(:i2-1) = s_temp(:i2-1)
-            s_temp2(i2:i2+size(s_ent)-1) = s_ent
+            s_temp2(i2:i2+len(s_ent)-1) = as_chars(s_ent)
             deallocate(s_temp)
             s_temp => s_temp2
             nullify(s_temp2)
             i = i + j + 1
-            i2 = i2 + size(s_ent)
-            deallocate(s_ent)
+            i2 = i2 + len(s_ent)
+            call destroy_vs(s_ent)
           else
             s_temp(i2:i2+j) = s_in(i:i+j)
             i = i + j + 1
@@ -968,17 +969,15 @@ contains
         endif
         deallocate(tempString)
       else
-        s_temp(i2) = s_in(i)
+        s_temp(i2) = s_in(i:i)
         i = i + 1
         i2 = i2 + 1
       endif
     enddo
 
-    deallocate(s_out)
-    allocate(s_out(i2-1))
-    s_out = s_temp(:i2-1)
+    call add_chars(s_out, str_vs(s_temp(:i2-1)))
 100 deallocate(s_temp)
-    if (associated(s_ent))  deallocate(s_ent)
+    if (associated(s_ent))  call destroy_vs(s_ent)
     if (associated(tempString)) deallocate(tempString)
 
   end function normalize_attribute_text

sax/m_sax_parser.F90
@@ -897,17 +897,10 @@ contains
         select case (fx%tokenType)
         case (TOK_CHAR)
           !First, expand all entities:
-          !FIXME: AMW - fix this nasty doubble casting. And the AMW temp sting is to avoid leaking memory (or haveing to deallocate inside of normalize function.
-          tempStringAMW => vs_str_alloc(as_chars(fx%token))
-          tempString => normalize_attribute_text(fx, tempStringAMW)
-          deallocate(tempStringAMW)
+          tempVString => normalize_attribute_text(fx, as_chars(fx%token))
           call destroy_vs(fx%token)
-          !FIXME: AMW - nasty casting!
-          fx%token => new_vs(init_chars=str_vs(tempString))
-          !FIXME: AMW - have to destroy tempString to avoid a memory leak
-          ! so the => null isn't needed at the mo - it will be later.
-          deallocate(tempString)
-          tempString => null()
+          fx%token => tempVString
+          tempVString => null()
           !If this attribute is not CDATA, we must process further;
           if (associated(attDecl)) then
             temp_i = attDecl%attType
@@ -1841,10 +1834,16 @@ contains
           if (processDTD) then
             do i = 1, size(elem%attlist%list)
               if (associated(elem%attlist%list(i)%default)) then
+                !FIXME: AMW Some nasty gymnastics here need removing,
+                !       but that will wait until the elem types are
+                !       converted to vstrs
                 tempString => elem%attlist%list(i)%default
+                tempVString => &
+                  normalize_attribute_text(fx, str_vs(tempString))
                 elem%attlist%list(i)%default => &
-                  normalize_attribute_text(fx, tempString)
+                  vs_str_alloc(as_chars(tempVString))
                 deallocate(tempString)
+                call destroy_vs(tempVString)
                 if (in_error(fx%error_stack)) return
               endif
             enddo

commit 72925dd33ac6bc3fed3e215c8e4a6639de70de5b

Fix mem leak introduced by 3e0390d530

sax/m_sax_tokenizer.F90
@@ -903,8 +903,7 @@ contains
           call add_error(fx%error_stack, "No entity reference found")
           goto 100
         endif
-        allocate(tempString(j-1))
-        tempString = vs_str_alloc(s_in(i+1:i+j-1))
+        tempString => vs_str_alloc(s_in(i+1:i+j-1))
         if (existing_entity(fx%predefined_e_list, str_vs(tempString))) then
           ! Expand immediately
           s_temp(i2) = expand_entity_text(fx%predefined_e_list, str_vs(tempString))