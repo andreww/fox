b7a1efb58719bae44cc21c114f0a4c1f6934b1e4
Andrew Walker <a.walker@ucl.ac.uk>
2009-07-20 15:56:07 +0100

    wkml: Tests for two documents in a file.

---
+++ wkml/test/test_kmlOpenCloseDocument_4.f90
@@ -0,0 +1,14 @@
+program test
+
+  use FoX_wkml
+
+  implicit none
+
+  type(xmlf_t) :: myfile
+
+  call kmlBeginFile(myfile, "test.xml", -1, .true., 'testdoc')
+  call kmlCloseDocument(myfile)
+  call kmlOpenDocument(myfile, "NewName", "anId")
+  call kmlFinishFile(myfile)
+
+end program test
