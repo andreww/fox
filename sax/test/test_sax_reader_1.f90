e22f1aeeb601bca6bcb4dc86e89f51f4185889e9
Andrew Walker <andrew.walker@bristol.ac.uk>
2012-04-24 21:11:19 +0100

    sax: Update test_sax_reader_1 for FoX 4.x
    Internal API has changed breaking this unit test.

--- sax/test/test_sax_reader_1.f90
+++ sax/test/test_sax_reader_1.f90
@@ -1,15 +1,18 @@
 program test_sax_reader
 
   use m_sax_reader
+  use m_common_error,  only: error_stack
 
   type(file_buffer_t) :: fb
 
+  type(error_stack) :: es
   integer :: iostat
-  character(1) :: c
+  logical :: eof
+  character(len=1) :: c
 
-  call open_file(fb, file="test_sax_reader_1.in",  iostat=iostat)
+  call open_file(fb, file="test_sax_reader_1.in",  iostat=iostat, es=es)
 
-  c = read_char(fb, iostat)
+  c = get_character(fb, eof, es)
   write(*,'(2a)') 'char:', c
   write(*,'(a,i0)') 'iost:', iostat
 
