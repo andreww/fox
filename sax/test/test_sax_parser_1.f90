475a570d3316886e3d3b377dcbdef200516d3f3d
Andrew Walker <andrew.walker@bristol.ac.uk>
2012-04-24 20:42:07 +0100

    sax: Update test_sax_parser_1 for FoX 4.x
    We cannot use the public sax interface to get hold
    of the encoding etc. So just run the parser (essentially
    a well formdness chedk).

--- sax/test/test_sax_parser_1.f90
+++ sax/test/test_sax_parser_1.f90
@@ -1,26 +1,15 @@
 program test_sax_reader
 
-  use m_common_array_str
-  use m_sax_parser
-  use m_sax_reader
-  use m_sax_types
-
-  type(file_buffer_t) :: fb
-  type(sax_parser_t) :: fx
-
+  use FoX_sax
+  type(xml_t) :: xp
   integer :: iostat
 
-  call open_file(fb, file="test_sax_fsm_1.in", iostat=iostat)
+  call open_xml_file(xp, "test_sax_fsm_1.in", iostat)
 
-  call sax_parser_init(fx, "test_sax_fsm_1.in")
+  write(*,'(i1)') iostat
 
-  call sax_parse(fx, fb)
+  call parse(xp)
 
-  write(*,'(i0)') fx%xds%xml_version
-  write(*,'(a)') str_vs(fx%xds%encoding)
-  write(*,'(l1)') fx%xds%standalone
-
-  call sax_parser_destroy(fx)
-  call close_file(fb) 
+  call close_xml_t(xp) 
 
 end program test_sax_reader
