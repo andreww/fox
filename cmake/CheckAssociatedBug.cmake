aed18dbfde64aaedd84c5b4a0f8389f504396e20
vsande <vsande@cimne.upc.edu>
2015-09-01 12:55:36 +0200

    Some minor changes in CMake compilation system:
        - Allows to select which targets will compile
        - Manage include_directories from the main CMakeLists.txt
        - Library names depend on project name

--- cmake/CheckAssociatedBug.cmake
+++ cmake/CheckAssociatedBug.cmake
@@ -3,7 +3,7 @@
 #=====================================================#
 
 MESSAGE(STATUS "Checking for 'associated in restricted expression' bug")
-TRY_COMPILE(TEST_ASSOCIATED ${fox_BINARY_DIR}/associated ${fox_SOURCE_DIR}/cmake
+TRY_COMPILE(TEST_ASSOCIATED ${${PROJECT_NAME}_BINARY_DIR}/associated ${${PROJECT_NAME}_SOURCE_DIR}/cmake
   fox_config expr_bug
   OUTPUT_VARIABLE BUILD_OUTPUT 
 )
